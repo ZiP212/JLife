fa3efe2f603d383461797dd62e01bb78840e6956
Pasha K-v <pasha.templar+github@gmail.com>
2014-08-10 19:52:47 +0300

    First commit ever

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pasha
+ * Date: 10.08.14
+ * Time: 19:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.exit(0);
+    }
+}
