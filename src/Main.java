a6949627bb80be55811331d3052c090d79f9b4e1
Pasha K-v <pasha.templar+github@gmail.com>
2014-08-10 20:16:18 +0300

    Added UI package

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import ui.MainFrame;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Pasha
@@ -6,8 +8,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
-        System.out.println("McDonalds");
-        System.exit(0);
+        MainFrame mainFrame = new MainFrame();
     }
 }
---
+++ src/ui/MainFrame.java
@@ -0,0 +1,10 @@
+package ui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pasha
+ * Date: 10.08.14
+ * Time: 20:14
+ */
+public class MainFrame {
+}
