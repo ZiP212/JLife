2c21c6e9c69a7e5e3fdb0c6f417850d61a0f5b46
Pasha K-v <pasha.templar+github@gmail.com>
2014-08-10 20:13:09 +0300

    reworked fastfood message

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world");
-        System.out.println("KFC");
+        System.out.println("McDonalds");
         System.exit(0);
     }
 }
