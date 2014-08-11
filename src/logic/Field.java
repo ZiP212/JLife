aaa0ed4ffb2e9e850383d6cea3d6a1a60dcece57
Pasha K-v <pasha.templar+github@gmail.com>
2014-08-11 16:16:52 +0300

    Added populate method to field class, also added CellOutOfBoundsException

--- src/logic/Field.java
+++ src/logic/Field.java
@@ -12,68 +12,77 @@ public class Field {
     private boolean[][] field;
 
     public Field(int height, int width) throws ZeroNegativeBoundsException {
-        if(height>0){
-            this.height=height;
-        }else{
-            throw new ZeroNegativeBoundsException("Height = "+height);
+        if (height > 0) {
+            this.height = height;
+        } else {
+            throw new ZeroNegativeBoundsException("Height = " + height);
         }
-        if(width>0){
-            this.width=width;
-        }else{
-            throw new ZeroNegativeBoundsException("Width = "+width);
+        if (width > 0) {
+            this.width = width;
+        } else {
+            throw new ZeroNegativeBoundsException("Width = " + width);
         }
         field = new boolean[height][width];
     }
 
-    public void flip(int x, int y) throws CellOutOfBounds{
-        if(x<0 || y<0 || x==height || y==width){
-            throw new CellOutOfBounds("x "+x+"; y "+y);
+    public void flip(int x, int y) throws CellOutOfBounds {
+        if (x < 0 || y < 0 || x == width || y == height) {
+            throw new CellOutOfBounds("x " + x + "; y " + y);
         }
         field[x][y] = !field[x][y];
     }
 
     public void step() throws CellOutOfBounds {
 
-        boolean[][] nextStep = new boolean[height][width];
-        for (int x = 0; x < height; x++) {
-            for (int y = 0; y < width; y++) {
-                int count=0;
-                if(field[x-1][y-1]==true)
-                    count++;
-                if(field[x][y-1]==true)
-                    count++;
-                if(field[x+1][y-1]==true)
-                    count++;
-                if(field[x-1][y]==true)
-                    count++;
-                if(field[x+1][y]==true)
-                    count++;
-                if(field[x-1][y+1]==true)
-                    count++;
-                if(field[x][y+1]==true)
-                    count++;
-                if(field[x+1][y+1]==true)
-                    count++;                     // cчитаем кол-во соседей(хз что будет при выходе за границы, например, для угловых клеток)
+        boolean[][] nextStep = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int count = 0;
 
-                if (field [x][y]==false)       // условие рождения
+                if (x>0 && y>0 && field[x - 1][y - 1]){
+                    count++;
+                }
+                if (y>0 && field[x][y - 1]){
+                    count++;
+                }
+                if (x<width-1 && y>0 && field[x + 1][y - 1]){
+                    count++;
+                }
+                if (x>0 && field[x - 1][y]){
+                    count++;
+                }
+                if (x<width-1 && field[x + 1][y]){
+                    count++;
+                }
+                if (x>0 && y<height-1 && field[x - 1][y + 1]){
+                    count++;
+                }
+                if (y<height-1 && field[x][y + 1]){
+                    count++;
+                }
+                if (x<width-1 && y<height-1 && field[x + 1][y + 1]){
+                    count++;
+                }
+
+                if (field[x][y] == false)       // условие рождения
                 {
-                    if(count==3)
-                        flip(x,y);
+                    if (count == 3) {
+                        nextStep[x][y] = true;
+                    }
+
+                } else                           // условия смерти
+                {
+                    if (count < 2 || count > 3) {
+                        nextStep[x][y] = false;
+                    }
 
                 }
-                else                           // условия смерти
-                {
-                     if(count<2||count>3)
-                         flip(x,y);
-
-                }
-                //TODO: add checks
             }
         }
 
-        for (int x = 0; x < height; x++) {
-            for (int y = 0; y < width; y++) {
-                field[x][y]=nextStep[x][y];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                field[x][y] = nextStep[x][y];
             }
         }
     }
